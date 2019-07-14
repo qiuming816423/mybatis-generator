ac80aafd9277f2daa85915302c7436ff4b39415f
qiuming <972040417@qq.com>
2019-07-15 00:07:06 +0800

    mybatis-generator

--- src/main/java/com/qiuming/mybatisgenerator/service/area/IAreaService.java
+++
@@ -1,7 +0,0 @@
-package com.qiuming.mybatisgenerator.service.area;
-
-import java.util.List;
-import com.qiuming.mybatisgenerator.bean.area.Area;
-public interface IAreaService {
-
-List<Area> getAreaList();}
